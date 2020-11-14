cc96b4de7512b45cdf11eed9888cf16f26338b75
shemaM <azertshema@gmail.com>
2020-11-14 11:52:05 +0200

    ch(login-and-logout):using firebase auth for login and logout a signin user

--- game_guidez/scripts/auth.js
+++ game_guidez/scripts/auth.js
@@ -1,3 +1,19 @@
+// listern for auth status changes
+
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    db.collection("guides")
+      .get()
+      .then((snapshot) => {
+        setupGuides(snapshot.docs);
+      });
+    setupUI(user);
+  } else {
+    setupUI();
+    setupGuides([]);
+  }
+});
+
 const signUpform = document.querySelector("#signup-form");
 
 signUpform.addEventListener("submit", (e) => {
@@ -12,5 +28,35 @@ signUpform.addEventListener("submit", (e) => {
     const modal = document.querySelector("#modal-signup");
     M.Modal.getInstance(modal).close();
     signUpform.reset();
+  });
+});
+
+//  signout a user
+
+const logout = document.querySelector("#logout");
+logout.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  auth.signOut().then(() => {
+    console.log("user signed out");
+  });
+});
+
+// login
+
+const loginForm = document.querySelector("#login-form");
+
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // logout
+  const email = loginForm["login-email"].value;
+  const password = loginForm["login-password"].value;
+  auth.signInWithEmailAndPassword(email, password).then((cred) => {
+    console.log(cred.user);
+    // closing session
+    const modal = document.querySelector("#modal-login");
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
   });
 });
